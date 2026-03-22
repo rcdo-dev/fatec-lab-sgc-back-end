f6cb3429f1d192c95e92e45d734eb3fa2b61683a
rcdo-dev <rcdo.dev@gmail.com>
2026-03-22 11:12:39 -0300

    fix: import removed.

--- src/main/java/br/com/sgc/api/cemetery/controller/BlockController.java
+++ src/main/java/br/com/sgc/api/cemetery/controller/BlockController.java
@@ -3,7 +3,6 @@ package br.com.sgc.api.cemetery.controller;
 import java.net.URI;
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
