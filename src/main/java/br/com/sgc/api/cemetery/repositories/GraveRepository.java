1cc2c206caefcbe5b2e7fdb703d36e9b88080d4b
rcdo-dev <rcdo.dev@gmail.com>
2026-03-23 13:44:41 -0300

    feat: Rule for maximum body capacity implemented, grave type and burial area validation implemented, method for saving a created grave updated.

--- src/main/java/br/com/sgc/api/cemetery/repositories/GraveRepository.java
+++ src/main/java/br/com/sgc/api/cemetery/repositories/GraveRepository.java
@@ -8,4 +8,6 @@ import br.com.sgc.api.cemetery.entity.GraveEntity;
 @Repository
 public interface GraveRepository extends JpaRepository<GraveEntity, Long> {
 
+    boolean existsByNumberAndBlockId(int number, Long blockId);
+
 }
--- src/main/java/br/com/sgc/api/cemetery/service/GraveService.java
+++ src/main/java/br/com/sgc/api/cemetery/service/GraveService.java
@@ -9,6 +9,9 @@ import br.com.sgc.api.cemetery.dto.response.GraveResponseDTO;
 import br.com.sgc.api.cemetery.mapper.GraveMapper;
 import br.com.sgc.api.cemetery.repositories.BlockRepository;
 import br.com.sgc.api.cemetery.repositories.GraveRepository;
+import br.com.sgc.api.common.enums.AreaType;
+import br.com.sgc.api.common.enums.GraveType;
+import br.com.sgc.api.common.exception.BusinessException;
 import br.com.sgc.api.common.exception.ResourceNotFoundException;
 
 import lombok.RequiredArgsConstructor;
@@ -21,6 +24,14 @@ public class GraveService {
     private final GraveMapper mapper;
 
     public GraveResponseDTO save(GraveRequestDTO request) {
+
+        validateAreaTypeAndGraveType(request.areaType(), request.graveType());
+        validateBodyCapacity(request.graveType(), request.bodyCapacity());
+
+        if (graveRepository.existsByNumberAndBlockId(request.number(), request.blockId())) {
+            throw new BusinessException("Já existe uma sepultura com essse número nessa quadra.");
+        }
+
         var blockEntity = blockRepository
                 .findById(Objects.requireNonNull(request.blockId(), "O Id da quadra não pode ser nulo"))
                 .orElseThrow(() -> new ResourceNotFoundException("Quadra não encontrada."));
@@ -29,6 +40,22 @@ public class GraveService {
         graveEntity.setBlock(blockEntity);
 
         return mapper.toResponse(graveRepository.save(graveEntity));
+    }
+
+    private void validateAreaTypeAndGraveType(AreaType areaType, GraveType graveType) {
+        if (areaType == AreaType.COMMON && graveType != GraveType.EARTH) {
+            throw new BusinessException("Sepultura de área comum deve ser do tipo terra.");
+        }
+    }
+
+    private void validateBodyCapacity(GraveType graveType, int bodyCapacity) {
+        if (graveType == GraveType.EARTH && bodyCapacity > 2) {
+            throw new BusinessException("Sepultura do tipo terra deve ter capacidade máxima de 2 corpos.");
+        }
+
+        if (graveType == GraveType.MAUSOLEUM && bodyCapacity > 4) {
+            throw new BusinessException("Jazigo deve ter capacidade máxima de 4 corpos.");
+        }
     }
 
 }
