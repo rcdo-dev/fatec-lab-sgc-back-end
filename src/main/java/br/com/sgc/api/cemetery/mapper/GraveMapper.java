c2fb65f23354b2c6d4258d1fc1e31955ec10ea1b
rcdo-dev <rcdo.dev@gmail.com>
2026-03-23 11:33:32 -0300

    feat: Mapper to GraveEntity created.

---
+++ src/main/java/br/com/sgc/api/cemetery/mapper/GraveMapper.java
@@ -0,0 +1,19 @@
+package br.com.sgc.api.cemetery.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import br.com.sgc.api.cemetery.dto.request.GraveRequestDTO;
+import br.com.sgc.api.cemetery.dto.response.GraveResponseDTO;
+import br.com.sgc.api.cemetery.entity.GraveEntity;
+
+@Mapper(componentModel = "spring")
+public interface GraveMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "block", ignore = true)
+    GraveEntity toEntity(GraveRequestDTO dto);
+
+    @Mapping(target = "blockId", source = "block.id")
+    GraveResponseDTO toResponse(GraveEntity entity);
+}
