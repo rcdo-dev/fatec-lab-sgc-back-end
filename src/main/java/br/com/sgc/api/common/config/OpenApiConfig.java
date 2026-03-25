bef9db90db052d9a1e210cddaa56fd3d33aa1771
rcdo-dev <rcdo.dev@gmail.com>
2026-03-25 16:28:29 -0300

    feat: CustomOpenAPI created.

--- src/main/java/br/com/sgc/api/common/config/OpenApiConfig.java
+++ src/main/java/br/com/sgc/api/common/config/OpenApiConfig.java
@@ -1,5 +1,30 @@
 package br.com.sgc.api.common.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
 public class OpenApiConfig {
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SGC API")
+                        .description("API Back-end do Sistema de Gerenciamento de Cemitérios")
+                        .version("v1")
+                        .contact(new Contact()
+                                .name("Equipe SGC")
+                                .email("contato@email.com"))
+                        .license(new License()
+                                .name("Uso acadêmico e institucional")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Documentação do projeto SGC"));
+    }
 }
