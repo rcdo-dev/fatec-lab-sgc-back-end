74bc9478925265d985a222c39eb92a5cb9af37e6
rcdo-dev <rcdo.dev@gmail.com>
2026-04-07 20:40:30 -0300

    feat: GraveRequestDTO update with message.properties.

--- src/main/java/br/com/sgc/api/cemetery/dto/request/GraveRequestDTO.java
+++ src/main/java/br/com/sgc/api/cemetery/dto/request/GraveRequestDTO.java
@@ -11,14 +11,25 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(title = "Grave Request", description = "DTO de entrada para cadastro de sepultura.")
 public record GraveRequestDTO(
 
-        @Schema(description = "Número da sepultura.", example = "127") @Positive int number,
+        @Schema(description = "Número da sepultura.", example = "127")
+        @Positive(message = "{validation.required.positive.integer}")
+        int number,
 
-        @Schema(description = "Tipo da sepultura (EARTH / MAUSOLEUM).", example = "EARTH") @NotNull GraveType graveType,
+        @Schema(description = "Tipo da sepultura (EARTH / MAUSOLEUM).", example = "EARTH")
+        @NotNull(message = "validation.assigned.value.cannot.be.null")
+        GraveType graveType,
 
-        @Schema(description = "Quantidade de corpos suportados pela sepultura.", example = "2") @Positive int bodyCapacity,
+        @Schema(description = "Quantidade de corpos suportados pela sepultura.", example = "2")
+        @Positive(message = "{validation.required.positive.integer}")
+        int bodyCapacity,
 
-        @Schema(description = "Tipo de área da sepultura (COMMON / PERPETUAL).", example = "COMMON") @NotNull AreaType areaType,
+        @Schema(description = "Tipo de área da sepultura (COMMON / PERPETUAL).", example = "COMMON")
+        @NotNull(message = "validation.assigned.value.cannot.be.null")
+        AreaType areaType,
 
-        @Schema(description = "ID da quadra em que a sepultura está vinculada.", example = "1") @NotNull @Positive Long blockId) {
+        @Schema(description = "ID da quadra em que a sepultura está vinculada.", example = "1")
+        @NotNull(message = "validation.assigned.value.cannot.be.null")
+        @Positive(message = "{validation.required.positive.integer}")
+        Long blockId) {
 
 }
