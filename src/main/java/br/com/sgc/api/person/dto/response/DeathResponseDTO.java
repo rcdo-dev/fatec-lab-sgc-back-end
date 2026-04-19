3a7155012004b7acd003f2cd7fa71af49b0fe2ac
rcdo-dev <rcdo.dev@gmail.com>
2026-04-19 15:15:46 -0300

    feat: DeathReponseDTO implemented.

--- src/main/java/br/com/sgc/api/person/dto/response/DeathResponseDTO.java
+++ src/main/java/br/com/sgc/api/person/dto/response/DeathResponseDTO.java
@@ -1,47 +1,42 @@
-package br.com.sgc.api.person.dto.request;
+package br.com.sgc.api.person.dto.response;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-import br.com.sgc.api.person.entity.DeceasedEntity;
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
-import jakarta.validation.constraints.Positive;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@Schema(title = "Death Request", description = "DTO de entrada para cadastro do óbito.")
-public record DeathRequestDTO(
+import br.com.sgc.api.person.entity.DeceasedEntity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(title = "Death Response", description = "DTO de resposta para o óbito.")
+@JsonPropertyOrder({"id", "place", "date", "time", "causeDeath", "doctorResponsible", "certificateNumber", "observations", "deceasedId"})
+public record DeathResponseDTO(
+    @Schema(description = "Identificador único do falecido.", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
+    Long id,
+
     @Schema(description = "Local do óbito.", example = "Apartamento")
-    @NotBlank(message = "{validation.text.cannot.be.blank.or.null}")
     String place,
 
     @Schema(description = "Data do óbito.", example = "2013-03-06")
-    @NotNull(message = "{validation.assigned.value.cannot.be.null}")
-    @PastOrPresent(message = "{validation.date.cannot.be.in.the.future}")
     LocalDate date,
 
     @Schema(description = "Hora do óbito", example = "05:30")
-    @NotNull(message = "{validation.assigned.value.cannot.be.null}")
     LocalTime time,
 
     @Schema(description = "Causa da morte", example = "Overdose.")
-    @NotBlank(message = "{validation.text.cannot.be.blank.or.null}")
     String causeDeath,
 
     @Schema(description = "Nome do médico responsável.", example = "Dr. Samu")
-    @NotBlank(message = "{validation.text.cannot.be.blank.or.null}")
     String doctorResponsible,
 
     @Schema(description = "Número da certidão de óbito.", example = "01234 / Não consta")
-    @NotBlank(message = "{validation.text.cannot.be.blank.or.null}")
     String certificateNumber,
 
     @Schema(description = "Campo para observações do óbito", example = "Poderia estar vivo e cantando.")
     String observations,
 
     @Schema(description = "Indica a qual falecido este óbito pertence.", example = "1")
-    @Positive(message = "{validation.required.positive.integer}")
     DeceasedEntity deceasedId
 ) {
 
