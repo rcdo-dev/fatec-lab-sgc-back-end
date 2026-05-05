837f079658067c4f2d821e6de6d6165e4b98cca3
rcdo-dev <rcdo.dev@gmail.com>
2026-05-05 09:32:38 -0300

    feat: DeceasedUnidentifiedMapper created.

---
+++ src/main/java/br/com/sgc/api/person/mapper/DeceasedUnidentifiedMapper.java
@@ -0,0 +1,19 @@
+package br.com.sgc.api.person.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import br.com.sgc.api.person.dto.request.DeceasedUnidentifiedRequestDTO;
+import br.com.sgc.api.person.dto.response.DeceasedUnidentifiedResponseDTO;
+import br.com.sgc.api.person.entity.DeceasedUnidentifiedEntity;
+
+@Mapper(componentModel = "spring")
+public interface DeceasedUnidentifiedMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "declarant", ignore = true)
+    DeceasedUnidentifiedEntity toEntity(DeceasedUnidentifiedRequestDTO dto);
+
+    @Mapping(target = "declarantId", source = "declarant.id")
+    DeceasedUnidentifiedResponseDTO toResponse(DeceasedUnidentifiedEntity entity);
+}
