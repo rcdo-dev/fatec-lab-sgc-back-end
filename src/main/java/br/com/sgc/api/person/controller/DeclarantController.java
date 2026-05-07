82dd0c20ab48b61359e6f1ec97d4add87e6fdf9e
rcdo-dev <rcdo.dev@gmail.com>
2026-05-07 13:29:18 -0300

    feat: The Declarant entity's operations are functioning.

--- src/main/java/br/com/sgc/api/person/controller/DeclarantController.java
+++ src/main/java/br/com/sgc/api/person/controller/DeclarantController.java
@@ -1,5 +1,15 @@
 package br.com.sgc.api.person.controller;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -7,15 +17,12 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import br.com.sgc.api.person.dto.request.DeclarantRequestDTO;
 import br.com.sgc.api.person.dto.response.DeclarantResponseDTO;
 import br.com.sgc.api.person.service.DeclarantService;
+
 import io.swagger.v3.oas.annotations.tags.Tag;
+
 import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
-
-import java.net.URI;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,14 +35,31 @@ public class DeclarantController {
     public ResponseEntity<DeclarantResponseDTO> create(@Valid @RequestBody DeclarantRequestDTO request) {
         var response = declarantService.save(request);
 
-        // Travar declarantes repetidos.
-        
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(response.id())
-                .toUri();
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(response.id()).toUri();
 
         return ResponseEntity.created(uri).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DeclarantResponseDTO>> findAll() {
+        return ResponseEntity.ok(declarantService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DeclarantResponseDTO> findById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(declarantService.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DeclarantResponseDTO> update(@PathVariable("id") Long id,
+            @Valid @RequestBody DeclarantRequestDTO request) {
+        return ResponseEntity.ok(declarantService.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<DeclarantResponseDTO> delete(@PathVariable("id") Long id) {
+        declarantService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
